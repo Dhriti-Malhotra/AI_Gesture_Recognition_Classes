ce76d57451348621cc2c9cb57196fbf89912d863
Dhriti-Malhotra <134493853+Dhriti-Malhotra@users.noreply.github.com>
2024-03-06 17:51:44 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -18,6 +18,10 @@ function modeLoaded()
 function draw()
 {
     background('#D8BFD8');
+    document.getElementById("square_side").innerHTML = "Width And Height of a square will be = " + difference + "px";
+    fill('#CD5C5C');
+    stroke('#F08080');
+    square(noseX, noseY, difference);
 }
 
 function gotPoses(results)
@@ -25,5 +29,20 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
     }
 }
+
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
